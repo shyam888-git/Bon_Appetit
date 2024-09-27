c84fd4c2a353207dbb4b77c23e56461c40d48b39
Shyam Sharki <ssk66221@gmail.com>
2024-09-27 22:16:46 +0545

    carousel

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -48,36 +48,6 @@ const Carousel = ({ images }) => {
           </div>
         ))}
       </div>
-
-      <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        {images.map((_, index) => (
-          <button
-            key={index}
-            type="button"
-            className={`w-3 h-3 rounded-full ${
-              index === currentSlide ? "bg-white" : "bg-white/50"
-            }`}
-            aria-current={index === currentSlide}
-            aria-label={`Slide ${index + 1}`}
-            onClick={() => goToSlide(index)}
-          ></button>
-        ))}
-      </div>
-
-      <button
-        type="button"
-        className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={goToPrevSlide}
-      >
-    
-      </button>
-      <button
-        type="button"
-        className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        onClick={goToNextSlide}
-      >
-       
-      </button>
     </div>
   );
 };
